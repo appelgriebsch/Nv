8ba32f9e91ac01d8dd81d4f38cc76f905e5d3329
Andreas Gerlach <andreas.gerlach@xarvio.com>
2023-01-11 12:05:34 +0100

    fix telescope extension settings; improve icons in symbols-outline view

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -73,21 +73,6 @@ return {
           height = 0.9,
           width = 0.9
         },
-        extensions = {
-          project = {
-            base_dirs = {
-              '~/Projects'
-            }
-          },
-          undo = {
-            use_delta = true,
-            side_by_side = true,
-            layout_strategy = "vertical",
-            layout_config = {
-              preview_height = 0.4,
-            },
-          },
-        },
         mappings = {
           i = {
             ["<C-j>"] = function(...)
@@ -117,6 +102,21 @@ return {
               return require("telescope.actions.layout").toggle_preview(...)
             end
           }
+        },
+      },
+      extensions = {
+        project = {
+          base_dirs = {
+            '~/Projects'
+          }
+        },
+        undo = {
+          use_delta = true,
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.4,
+          },
         },
       },
     },
@@ -172,7 +172,39 @@ return {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = true,
+    config = function()
+      local icons = require("lazyvim.config").icons
+      require("symbols-outline").setup({
+        symbols = {
+          File = { icon = icons.kinds.File, hl = "TSURI" },
+          Module = { icon = icons.kinds.Module, hl = "TSNamespace" },
+          Namespace = { icon = icons.kinds.Namespace, hl = "TSNamespace" },
+          Package = { icon = icons.kinds.Package, hl = "TSNamespace" },
+          Class = { icon = icons.kinds.Class, hl = "TSType" },
+          Method = { icon = icons.kinds.Method, hl = "TSMethod" },
+          Property = { icon = icons.kinds.Property, hl = "TSMethod" },
+          Field = { icon = icons.kinds.Field, hl = "TSField" },
+          Constructor = { icon = icons.kinds.Constructor, hl = "TSConstructor" },
+          Enum = { icon = icons.kinds.Enum, hl = "TSType" },
+          Interface = { icon = icons.kinds.Interface, hl = "TSType" },
+          Function = { icon = icons.kinds.Function, hl = "TSFunction" },
+          Variable = { icon = icons.kinds.Variable, hl = "TSConstant" },
+          Constant = { icon = icons.kinds.Constant, hl = "TSConstant" },
+          String = { icon = icons.kinds.String, hl = "TSString" },
+          Number = { icon = icons.kinds.Number, hl = "TSNumber" },
+          Boolean = { icon = icons.kinds.Boolean, hl = "TSBoolean" },
+          Array = { icon = icons.kinds.Array, hl = "TSConstant" },
+          Object = { icon = icons.kinds.Object, hl = "TSType" },
+          Key = { icon = icons.kinds.Key, hl = "TSType" },
+          Null = { icon = icons.kinds.Null, hl = "TSType" },
+          EnumMember = { icon = icons.kinds.EnumMember, hl = "TSField" },
+          Struct = { icon = icons.kinds.Struct, hl = "TSType" },
+          Event = { icon = icons.kinds.Event, hl = "TSType" },
+          Operator = { icon = icons.kinds.Operator, hl = "TSOperator" },
+          TypeParameter = { icon = icons.kinds.TypeParameter, hl = "TSParameter" }
+        }
+      })
+    end
   },
 
   -- add zen-mode

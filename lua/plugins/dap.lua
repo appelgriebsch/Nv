0b2af6a743af922646b6f859cf9e15115da67d7e
Andreas Gerlach <andreas.gerlach@xarvio.com>
2023-01-11 16:18:46 +0100

    additional icons for dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -12,6 +12,7 @@ return {
     config = function()
       vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
       vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "", linehl = "", numhl = "" })
       vim.fn.sign_define("DapLogPoint", { text = "", texthl = "", linehl = "", numhl = "" })
       vim.fn.sign_define("DapStopped", { text = "", texthl = "", linehl = "", numhl = "" })
       require("dap").defaults.fallback.terminal_win_cmd = "enew"

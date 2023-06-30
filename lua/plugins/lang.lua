dd07f5f0ea6527dd7554feb56a77ab9cefede180
Andreas Gerlach <andreas.gerlach@xarvio.com>
2023-06-30 12:24:12 +0200

    switch to LazyVim Rust extra plugin

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -72,11 +72,11 @@ return {
   { import = "lazyvim.plugins.extras.dap.nlua" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
   -- custom language specific extension modules
   { import = "plugins.extras.lang.java" },
-  { import = "plugins.extras.lang.rust" },
   { import = "plugins.extras.lang.nodejs" },
 
 }

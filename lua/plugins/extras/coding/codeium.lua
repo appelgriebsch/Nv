a725d2d9a2e648780afd78f4e744c62a926e58a5
Andreas Gerlach <andi@appelgriebsch.org>
2023-08-06 21:25:51 +0200

    update codeium keymaps

--- lua/plugins/extras/coding/codeium.lua
+++ lua/plugins/extras/coding/codeium.lua
@@ -4,11 +4,10 @@ return {
     "Exafunction/codeium.vim",
     event = "VeryLazy",
     config = function(_, opts)
-      -- Change "<C-g>" here to any keycode you like.
-      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("i", "<C-a>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<C-h>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<C-l>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
     end
   },
 

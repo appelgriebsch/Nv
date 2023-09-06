a88a39d57f20c1739c95a2d136c9a4fce19f91c2
Andreas Gerlach <andreas.gerlach@xarvio.com>
2023-09-06 12:17:56 +0200

    update Java options to new LazyVim Java extension

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -85,75 +85,76 @@ return {
     "mfussenegger/nvim-jdtls",
     optional = true,
     opts = {
-      settings = {
-        java = {
-          configuration = {
-            updateBuildConfiguration = "automatic",
-          },
-          codeGeneration = {
-            toString = {
-              template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+      jdtls = {
+        settings = {
+          java = {
+            configuration = {
+              updateBuildConfiguration = "automatic",
             },
-            useBlocks = true,
-          },
-          completion = {
-            favoriteStaticMembers = {
-              "org.assertj.core.api.Assertions.*",
-              "org.junit.Assert.*",
-              "org.junit.Assume.*",
-              "org.junit.jupiter.api.Assertions.*",
-              "org.junit.jupiter.api.Assumptions.*",
-              "org.junit.jupiter.api.DynamicContainer.*",
-              "org.junit.jupiter.api.DynamicTest.*",
-              "org.mockito.Mockito.*",
-              "org.mockito.ArgumentMatchers.*",
-              "org.mockito.Answers.*"
+            codeGeneration = {
+              toString = {
+                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+              },
+              useBlocks = true,
             },
-            importOrder = {
-              "#",
-              "java",
-              "javax",
-              "org",
-              "com"
+            completion = {
+              favoriteStaticMembers = {
+                "org.assertj.core.api.Assertions.*",
+                "org.junit.Assert.*",
+                "org.junit.Assume.*",
+                "org.junit.jupiter.api.Assertions.*",
+                "org.junit.jupiter.api.Assumptions.*",
+                "org.junit.jupiter.api.DynamicContainer.*",
+                "org.junit.jupiter.api.DynamicTest.*",
+                "org.mockito.Mockito.*",
+                "org.mockito.ArgumentMatchers.*",
+                "org.mockito.Answers.*"
+              },
+              importOrder = {
+                "#",
+                "java",
+                "javax",
+                "org",
+                "com"
+              },
             },
-          },
-          contentProvider = { preferred = "fernflower" },
-          eclipse = {
-            downloadSources = true,
-          },
-          flags = {
-            allow_incremental_sync = true,
-            server_side_fuzzy_completion = true
-          },
-          implementationsCodeLens = {
-            enabled = false, --Don"t automatically show implementations
-          },
-          inlayHints = {
-            parameterNames = { enabled = "all" }
-          },
-          maven = {
-            downloadSources = true,
-          },
-          referencesCodeLens = {
-            enabled = false, --Don"t automatically show references
-          },
-          references = {
-            includeDecompiledSources = true,
-          },
-          saveActions = {
-            organizeImports = true,
-          },
-          signatureHelp = { enabled = true },
-          sources = {
-            organizeImports = {
-              starThreshold = 9999,
-              staticStarThreshold = 9999,
+            contentProvider = { preferred = "fernflower" },
+            eclipse = {
+              downloadSources = true,
+            },
+            flags = {
+              allow_incremental_sync = true,
+              server_side_fuzzy_completion = true
+            },
+            implementationsCodeLens = {
+              enabled = false, --Don"t automatically show implementations
+            },
+            inlayHints = {
+              parameterNames = { enabled = "all" }
+            },
+            maven = {
+              downloadSources = true,
+            },
+            referencesCodeLens = {
+              enabled = false, --Don"t automatically show references
+            },
+            references = {
+              includeDecompiledSources = true,
+            },
+            saveActions = {
+              organizeImports = true,
+            },
+            signatureHelp = { enabled = true },
+            sources = {
+              organizeImports = {
+                starThreshold = 9999,
+                staticStarThreshold = 9999,
+              },
             },
           },
         },
       },
     },
-    config = function() end
   },
 
 }

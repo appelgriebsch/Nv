3e54e36f5cd5aee9a88efca0b0b1bdc88c84396f
Andreas Gerlach <andreas.gerlach@xarvio.com>
2023-11-20 10:21:14 +0100

    make project storing folders again automatically

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -145,4 +145,19 @@ return {
       end
     end
   },
+
+  -- project
+  {
+    "ahmedkhalf/project.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.manual_mode = false
+      opts.detection_methods = { "lsp", "pattern" }
+      opts.patterns = {
+        ".git",
+        ".hg",
+        ".svn",
+      }
+    end
+  },
 }

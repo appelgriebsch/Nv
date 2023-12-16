6932d3140db6001102b7c51f6666963bb982e2b7
Andreas Gerlach <andi@appelgriebsch.org>
2023-12-16 20:34:52 +0100

    fix options for Rust

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -48,12 +48,16 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^3", -- Recommended
     ft = { "rust" },
-    keys = {
-      { "<leader>cR", function() vim.cmd.RustLsp("codeAction") end, desc = "Code Action" },
-      { "<leader>dr", function() vim.cmd.RustLsp("debuggables") end, desc = "Rust debuggables" },
-    },
     opts = {
       server = {
+        on_attach = function(client, bufnr)
+          -- register which-key mappings
+          local wk = require("which-key")
+          wk.register({
+            ["<leader>cR"] = { function() vim.cmd.RustLsp("codeAction") end, "Code Action" },
+            ["<leader>dr"] = { function() vim.cmd.RustLsp("debuggables") end, "Rust debuggables" },
+          }, { mode = "n", buffer = bufnr })
+        end,
         settings = {
           -- rust-analyzer language server configuration
           ["rust-analyzer"] = {

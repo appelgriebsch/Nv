29c119e29d7bc81ce69ea001945a00219f2c2a93
Andreas Gerlach <andi@appelgriebsch.org>
2023-12-16 18:12:17 +0100

    update rustacean extension, allowing to overwrite server options

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -48,8 +48,36 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^3", -- Recommended
     ft = { "rust" },
+    opts = {
+      server = {
+        settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      }
+    },
     config = function(_, opts)
-      vim.g.rustaceanvim = {
+      vim.g.rustaceanvim = vim.tbl_deep_extend("force", {
         -- LSP configuration
         server = {
           on_attach = function(client, bufnr)
@@ -60,32 +88,8 @@ return {
               ["<leader>dr"] = { function() vim.cmd.RustLsp("debuggables") end, "Rust debuggables" },
             }, { mode = "n", buffer = bufnr })
           end,
-          settings = {
-            -- rust-analyzer language server configuration
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true,
-                loadOutDirsFromCheck = true,
-                runBuildScripts = true,
-              },
-              -- Add clippy lints for Rust.
-              checkOnSave = {
-                allFeatures = true,
-                command = "clippy",
-                extraArgs = { "--no-deps" },
-              },
-              procMacro = {
-                enable = true,
-                ignored = {
-                  ["async-trait"] = { "async_trait" },
-                  ["napi-derive"] = { "napi" },
-                  ["async-recursion"] = { "async_recursion" },
-                },
-              },
-            },
-          },
         },
-      }
+      }, opts or {})
     end
   },
 

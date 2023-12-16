50c53a310f8f3b2d7ac7da74a146a7360e4b9f90
Andreas Gerlach <andi@appelgriebsch.org>
2023-12-16 16:40:46 +0100

    fix terminal.open / root.get for custom terminal commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 if vim.fn.executable("gitui") == 1 then
   -- gitui instead of lazygit
-  vim.keymap.set("n", "<leader>gG", function() require("lazyvim.util").float_term({ "gitui" }, { esc_esc = false, ctrl_hjkl = false }) end, { desc = "gitui (cwd)" })
-  vim.keymap.set("n", "<leader>gg", function() require("lazyvim.util").float_term({ "gitui" }, { cwd = require("lazyvim.util").get_root(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "gitui (root dir)" })
+  vim.keymap.set("n", "<leader>gG", function() require("lazyvim.util").terminal.open({ "gitui" }, { esc_esc = false, ctrl_hjkl = false }) end, { desc = "gitui (cwd)" })
+  vim.keymap.set("n", "<leader>gg", function() require("lazyvim.util").terminal.open({ "gitui" }, { cwd = require("lazyvim.util").root.get(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "gitui (root dir)" })
 end
 
 if vim.fn.executable("btop") == 1 then
   -- btop
-  vim.keymap.set("n", "<leader>xb", function() require("lazyvim.util").float_term({ "btop" }, { esc_esc = false, ctrl_hjkl = false }) end, { desc = "btop" })
+  vim.keymap.set("n", "<leader>xb", function() require("lazyvim.util").terminal.open({ "btop" }, { esc_esc = false, ctrl_hjkl = false }) end, { desc = "btop" })
 end

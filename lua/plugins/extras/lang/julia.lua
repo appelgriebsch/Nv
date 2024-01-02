648402b644e42da45a4507fb9f33eeb883e3f109
Andreas Gerlach <andi@appelgriebsch.org>
2024-01-02 19:55:04 +0100

    add julia extension

---
+++ lua/plugins/extras/lang/julia.lua
@@ -0,0 +1,23 @@
+return {
+
+  -- add julia specific modules to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "julia" })
+      end
+    end,
+  },
+
+  -- correctly setup mason lsp extensions
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "julia-lsp" })
+      end
+    end,
+  },
+
+}

8039496798b63d2ccfa4d0f9529ef6b6eda12c96
Andreas Gerlach <andreas.gerlach@xarvio.com>
2024-07-10 12:22:56 +0200

    updates to custom plugins; use rest extension from upstream

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -160,4 +160,37 @@ return {
       }
     end
   },
+
+  -- rest
+  {
+    "mistweaverco/kulala.nvim",
+    optional = true,
+    opts = {
+      -- default_view, body or headers
+      default_view = "body",
+      -- dev, test, prod, can be anything
+      -- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
+      default_env = "dev",
+      -- enable/disable debug mode
+      debug = false,
+      -- default formatters for different content types
+      formatters = {
+        json = { "jq", "." },
+        xml = { "xmllint", "--format", "-" },
+        html = { "xmllint", "--format", "--html", "-" },
+      },
+      -- default icons
+      icons = {
+        inlay = {
+          loading = "󰔟 ",
+          done = " "
+        },
+        lualine = "󱂛",
+      },
+      -- additional cURL options
+      -- e.g. { "--insecure", "-A", "Mozilla/5.0" }
+      additional_curl_options = {},
+    },
+
+  }
 }

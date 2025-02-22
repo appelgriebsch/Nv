08243f88eaebeb3901282d7982c93b1a63dfc436
Andreas Gerlach <andreas.gerlach@xarvio.com>
2025-02-22 12:02:54 +0100

    revert back to telescope

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,8 +8,7 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
 -- then you need to set the option below.
-vim.g.lazyvim_picker = "snacks"
-vim.g.lazyvim_explorer = "snacks"
+vim.g.lazyvim_picker = "telescope"
 
 vim.cmd[[nnoremap <ScrollWheelRight> <Nop>]]
 vim.cmd[[nnoremap <ScrollWheelLeft> <Nop>]]

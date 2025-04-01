5d43871de182e8857bd585229f26d5d7ae193b7d
Andreas Gerlach <andreas.gerlach@xarvio.com>
2025-04-01 17:25:58 +0200

    let user decide which picker they want

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,10 +7,6 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
--- then you need to set the option below.
-vim.g.lazyvim_picker = "snacks"
-vim.g.lazyvim_explorer = "snacks"
-
 vim.cmd[[nnoremap <ScrollWheelRight> <Nop>]]
 vim.cmd[[nnoremap <ScrollWheelLeft> <Nop>]]
 vim.cmd[[nnoremap <S-ScrollWheelUp> <ScrollWheelRight>]]
